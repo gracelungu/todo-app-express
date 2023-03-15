e47b06304c0537786413193df9a2246a496901ba
Grace Lungu <gracelungu@Graces-MBP.fritz.box>
2023-03-15 13:47:10 +0100

    Add pagination to getAllItems endpoint

--- helpers/items.js
+++ helpers/items.js
@@ -5,8 +5,7 @@ const todoList = [
   ];
   
   // GET all items
-  const getAllItems = (req, res) => {
-    res.json(todoList);
+  const getAllItems = (req, res) => {const page = parseInt(req.query.page) || 1;const limit = parseInt(req.query.limit) || 10;const startIndex = (page - 1) * limit;const endIndex = page * limit;const paginatedItems = todoList.slice(startIndex, endIndex);res.json(paginatedItems);};
   };
   
   // GET single item
